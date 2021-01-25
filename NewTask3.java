8e981d155bbb1e9137ec9b9f46a88316e6cda84a
Maxim_Savchenko <ya.max11122@yandex.by>
2021-01-25 21:17:01 +0300

    NewTask
    NewTask

---
+++ NewTask3.java
@@ -0,0 +1,15 @@
+package Task_of_courses;
+
+import java.util.Scanner;
+
+public class NewTask3 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int a = x.nextInt();
+        int b = x.nextInt();
+        int c = x.nextInt();
+        System.out.println(a);
+        System.out.print(b);
+        System.out.println(c);
+    }
+}
---
+++ NewTask8.java
@@ -0,0 +1,8 @@
+package Task_of_courses;
+
+public class NewTask8 {
+    public static void main(String[] args) {
+        for (int x = 1; x<=10; x++)
+            System.out.println("3 * "+ x + " = " + x*3);
+    }
+}
